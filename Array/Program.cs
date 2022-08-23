611bf2d5ab2d32f82f9a41ae2bfbf3e4826451b2
Åke aa <ake.bje@privat.se>
2022-08-23 10:25:42 +0200

    "Some changes"

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Array
 {
@@ -11,17 +7,18 @@ namespace Array
         static void Main(string[] args)
         {
             string[] namn = new string[5];
-            namn[0] = "Alf";
-            namn[1] = "Jonas";
-            namn[2] = "Jesper";
-            namn[3] = "Kalle";
-            namn[4] = "Hobbe";
+            //namn[0] = "Alf";
+            //namn[1] = "Jonas";
+            //namn[2] = "Jesper";
+            //namn[3] = "Kalle";
+            //namn[4] = "Hobbe";
 
             for (int i = 0; i < namn.Length; i++)
             {
+                Console.WriteLine("Skriv namn på index " + i);
+                namn[i] = Console.ReadLine();
                 Console.WriteLine(namn[i]);
             }
-
 
 
             Console.ReadLine();
